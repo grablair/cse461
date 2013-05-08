abb2768de185cacf9118b80f1b3feb5801bd85b0
Bryan Martin <bryanmartin1@msn.com>
2013-05-07 22:51:06 -0700

    Implemented PingTCPMessageHandler and DataXferTCPMessageHandler

--- ConsoleApps/src/edu/uw/cs/cse461/consoleapps/solution/DataXferTCPMessageHandler.java
+++ ConsoleApps/src/edu/uw/cs/cse461/consoleapps/solution/DataXferTCPMessageHandler.java
@@ -1,27 +1,155 @@
 package edu.uw.cs.cse461.consoleapps.solution;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uw.cs.cse461.consoleapps.DataXferInterface.DataXferTCPMessageHandlerInterface;
+import edu.uw.cs.cse461.net.base.NetBase;
+import edu.uw.cs.cse461.net.base.NetLoadable.NetLoadableConsoleApp;
+import edu.uw.cs.cse461.net.tcpmessagehandler.TCPMessageHandler;
+import edu.uw.cs.cse461.service.DataXferServiceBase;
+import edu.uw.cs.cse461.util.ConfigManager;
+import edu.uw.cs.cse461.util.SampledStatistic.TransferRate;
 import edu.uw.cs.cse461.util.SampledStatistic.TransferRateInterval;
 
-public class DataXferTCPMessageHandler implements
-		DataXferTCPMessageHandlerInterface {
+public class DataXferTCPMessageHandler extends NetLoadableConsoleApp implements DataXferTCPMessageHandlerInterface {
+
+	public DataXferTCPMessageHandler() {
+		super("dataxfertcpmessagehandler");
+	}
 
 	@Override
-	public byte[] DataXfer(String header, String hostIP, int port, int timeout,
-			int xferLength) throws JSONException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+	public void run() throws Exception {
+		try {
+
+			// Eclipse doesn't support System.console()
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+			ConfigManager config = NetBase.theNetBase().config();
+			String server = config.getProperty("net.server.ip");
+			if ( server == null ) {
+				System.out.print("Enter a host ip, or exit to exit: ");
+				server = console.readLine();
+				if ( server == null ) return;
+				if ( server.equals("exit")) return;
+			}
+
+			System.out.print("Enter the server's port number, or empty line to exit: ");
+			String targetPortStr = console.readLine();
+			if ( targetPortStr == null || targetPortStr.trim().isEmpty() ) return;
+			int targetPort = Integer.parseInt( targetPortStr );
+			
+			int socketTimeout = config.getAsInt("net.timeout.socket", -1);
+			if ( socketTimeout < 0 ) {
+				System.out.print("Enter socket timeout (in msec.): ");
+				String timeoutStr = console.readLine();
+				socketTimeout = Integer.parseInt(timeoutStr);
+				
+			}
+
+			System.out.print("Enter number of trials: ");
+			String trialStr = console.readLine();
+			int nTrials = Integer.parseInt(trialStr);
+			
+			System.out.print("Enter the ammount of data to transfer, or empty line to exit: ");
+			String xferLengthStr = console.readLine();
+			if ( xferLengthStr == null || xferLengthStr.trim().isEmpty() ) return;
+			int xferLength = Integer.parseInt( xferLengthStr );
+			
+			TransferRate.clear();
+
+			System.out.println("\n" + xferLength + " bytes");
+
+			//-----------------------------------------------------
+			// TCP transfer
+			//-----------------------------------------------------
+
+			TransferRateInterval xferStats = DataXferRate(DataXferServiceBase.HEADER_STR, server, targetPort, socketTimeout, xferLength, nTrials);
+
+			System.out.println("\nTCP: xfer rate = " + String.format("%9.0f", xferStats.mean() * 1000.0) + " bytes/sec.");
+			System.out.println("TCP: failure rate = " + String.format("%5.1f", xferStats.failureRate()) +
+					" [" + xferStats.nAborted()+ "/" + xferStats.nTrials() + "]");
+
+			
+		} catch (Exception e) {
+			System.out.println("Unanticipated exception: " + e.getMessage());
+		}
+		
 	}
+	
 
 	@Override
 	public TransferRateInterval DataXferRate(String header, String hostIP,
 			int port, int timeout, int xferLength, int nTrials) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		for ( int trial=0; trial<nTrials; trial++) {
+			try {
+				TransferRate.start("xfer");
+				DataXfer(header, hostIP, port, timeout, xferLength);
+				TransferRate.stop("xfer", xferLength);
+			} catch (ConnectException e) {
+				TransferRate.abort("xfer", xferLength);
+			} catch (Exception e) {
+				TransferRate.abort("xfer", xferLength);
+				System.out.println("xfer trial failed: " + e.getMessage());
+			}
+		
+		}
+		return TransferRate.get("xfer");
 	}
+	
+	@Override
+	public byte[] DataXfer(String header, String hostIP, int port, int timeout,
+			int xferLength) throws JSONException, IOException {
+		Socket tcpSocket = null;
+		TCPMessageHandler tcpMessageHandlerSocket = null;
+		try {
+			// set up the Message Handler
+			tcpSocket = new Socket(hostIP, port);
+			tcpMessageHandlerSocket = new TCPMessageHandler(tcpSocket);
+			tcpMessageHandlerSocket.setTimeout(timeout);
+			tcpMessageHandlerSocket.setNoDelay(true);
+			
+			// send the header and message
+			tcpMessageHandlerSocket.sendMessage(header);
+			JSONObject lengthRequest = new JSONObject();
+			lengthRequest.put("transferSize", xferLength);
+			tcpMessageHandlerSocket.sendMessage(lengthRequest);
+			
+			// read response header
+			String headerStr = tcpMessageHandlerSocket.readMessageAsString();
+			if ( ! headerStr.equalsIgnoreCase(DataXferServiceBase.RESPONSE_OKAY_STR) )
+				throw new IOException("Bad response header: '" + headerStr + "'");
+
+			// read response
+			ByteBuffer message = ByteBuffer.allocate(xferLength);
+			int bytesRead = 0;
+			
+			while (bytesRead < xferLength) {
+				byte[] chunk = tcpMessageHandlerSocket.readMessageAsBytes();
+				message.put(chunk);
+				bytesRead += chunk.length;
+			}
+			
+			tcpMessageHandlerSocket.close();
+			return message.array();
+		} catch (ConnectException e) {
+			System.out.println("TCP connection refused");
+			throw e;
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.getMessage());
+			throw e;
+		} finally {
+			if ( tcpMessageHandlerSocket != null ) try {tcpMessageHandlerSocket.close();} catch (Exception e) {}
+		}
+	}
+	
 
 }
--- ConsoleApps/src/edu/uw/cs/cse461/consoleapps/solution/PingTCPMessageHandler.java
+++ ConsoleApps/src/edu/uw/cs/cse461/consoleapps/solution/PingTCPMessageHandler.java
@@ -1,15 +1,131 @@
 package edu.uw.cs.cse461.consoleapps.solution;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 import edu.uw.cs.cse461.consoleapps.PingInterface.PingTCPMessageHandlerInterface;
+import edu.uw.cs.cse461.net.base.NetBase;
+import edu.uw.cs.cse461.net.base.NetLoadable.NetLoadableConsoleApp;
+import edu.uw.cs.cse461.net.tcpmessagehandler.TCPMessageHandler;
+import edu.uw.cs.cse461.service.EchoServiceBase;
+import edu.uw.cs.cse461.util.ConfigManager;
+import edu.uw.cs.cse461.util.SampledStatistic.ElapsedTime;
 import edu.uw.cs.cse461.util.SampledStatistic.ElapsedTimeInterval;
 
-public class PingTCPMessageHandler implements PingTCPMessageHandlerInterface {
+public class PingTCPMessageHandler extends NetLoadableConsoleApp implements PingTCPMessageHandlerInterface {
+	
+	// ConsoleApp's must have a constructor taking no arguments
+	public PingTCPMessageHandler() {
+		super("pingtcpmessagehandler");
+	}
 
+	/* (non-Javadoc)
+	 * @see edu.uw.cs.cse461.ConsoleApps.PingInterface#run()
+	 */
 	@Override
-	public ElapsedTimeInterval ping(String header, String hostIP, int port,
-			int timeout, int nTrials) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public void run() {
+		try {
+			// Eclipse doesn't support System.console()
+			BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+			ConfigManager config = NetBase.theNetBase().config();
+
+			String targetIP = config.getProperty("net.server.ip");
+			if ( targetIP == null ) {
+				System.out.print("Enter the server's ip, or empty line to exit: ");
+				targetIP = console.readLine();
+				if ( targetIP == null || targetIP.trim().isEmpty() ) return;
+			}
+
+			System.out.print("Enter the server's TCP port, or empty line to exit: ");
+			String targetTCPPortStr = console.readLine();
+			if ( targetTCPPortStr == null || targetTCPPortStr.trim().isEmpty() ) return;
+			int targetTCPPort = Integer.parseInt( targetTCPPortStr );
+
+			System.out.print("Enter number of trials: ");
+			String trialStr = console.readLine();
+			int nTrials = Integer.parseInt(trialStr);
+
+			int socketTimeout = config.getAsInt("net.timeout.socket", 2000);
+
+			System.out.println("Host: " + targetIP);
+			System.out.println("tcp port: " + targetTCPPort);
+			System.out.println("trials: " + nTrials);
+
+			ElapsedTimeInterval pingResult = null;
+
+			if ( targetTCPPort != 0 ) {
+				ElapsedTime.clear();
+				pingResult = ping("ping", targetIP, targetTCPPort, socketTimeout, nTrials);
+			}
+
+			if ( pingResult != null ) System.out.println("PING: " + String.format("%.2f msec (%d failures)", pingResult.mean(), pingResult.nAborted()));
+
+		} catch (Exception e) {
+			System.out.println("PingTCPMessageHandler.run() caught exception: " + e.getMessage());
+		}
+	}
+	
+	
+	@Override
+	public ElapsedTimeInterval ping(String header, String hostIP, int port, int timeout, int nTrials) throws Exception {
+		
+		//TODO: implement this method
+		
+		try {
+
+			Socket tcpSocket = null;
+			TCPMessageHandler tcpMessageHandlerSocket = null;
+			String msg = "";
+			for(int i = 0 ; i < nTrials ; i++) {
+				try {
+					ElapsedTime.start("PingTCPMessageHandler_PingTotalDelay");
+					
+					// set up the Message Handler
+					tcpSocket = new Socket(hostIP, port);
+					tcpMessageHandlerSocket = new TCPMessageHandler(tcpSocket);
+					tcpMessageHandlerSocket.setTimeout(timeout);
+					tcpMessageHandlerSocket.setNoDelay(true);
+					
+					// send the header and message
+					tcpMessageHandlerSocket.sendMessage(header);
+					tcpMessageHandlerSocket.sendMessage(msg);
+
+					// read response header
+					String headerStr = tcpMessageHandlerSocket.readMessageAsString();
+					if ( ! headerStr.equalsIgnoreCase(EchoServiceBase.RESPONSE_OKAY_STR) )
+						throw new Exception("Bad response header: '" + headerStr + "'");
+
+					// read response payload (which should be empty)
+					String response = tcpMessageHandlerSocket.readMessageAsString();
+					if ( !  response.equals(msg))
+						throw new Exception("Bad response payload: sent '" + msg + "' got back '" + response + "'");
+				} catch (SocketTimeoutException e) {
+					System.out.println("Timed out");
+					ElapsedTime.abort("PingTCPMessageHandler_PingTotalDelay");
+				} catch (Exception e) {
+					System.out.println("TCPMessageHandler read failed: " + e.getMessage());
+					ElapsedTime.abort("PingTCPMessageHandler_PingTotalDelay");
+				} finally {
+					if ( tcpMessageHandlerSocket != null ) {
+						try {
+							tcpMessageHandlerSocket.close();
+							ElapsedTime.stop("PingTCPMessageHandler_PingTotalDelay");
+						} catch (Exception e) {
+							ElapsedTime.abort("PingTCPMessageHandler_PingTotalDelay");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.getMessage());
+			ElapsedTime.abort("PingTCPMessageHandler_PingTotalDelay");
+		}
+
+		return ElapsedTime.get("PingTCPMessageHandler_PingTotalDelay");
 	}
 
 }
