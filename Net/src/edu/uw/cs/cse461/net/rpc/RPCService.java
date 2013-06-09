a84f9aea3d5cb12382d0c6445b574a6557e1b017
Graham Arthur Blair <graham@grahamarthurblair.com>
2013-06-09 13:23:26 -0700

    Implemented service. Needs work.

--- Net/src/edu/uw/cs/cse461/net/rpc/RPCService.java
+++ Net/src/edu/uw/cs/cse461/net/rpc/RPCService.java
@@ -1,14 +1,21 @@
 package edu.uw.cs.cse461.net.rpc;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.uw.cs.cse461.net.base.NetBase;
@@ -32,6 +39,15 @@ import edu.uw.cs.cse461.util.Log;
 public class RPCService extends NetLoadableService implements Runnable, RPCServiceInterface {
 	private static final String TAG="RPCService";
 	
+	private final Map<Pair<String, String>, RPCCallableMethod> rpcMethods;
+	private ServerSocket mServerSocket = null;
+	private ExecutorService threadPool = null;
+	
+	private static final int NUM_THREADS = 40;
+	
+	private int currentId = 1;
+	private final String HOST;
+	
 	/**
 	 * Constructor.  Creates the Java ServerSocket and binds it to a port.
 	 * If the config file specifies an rpc.server.port value, it should be bound to that port.
@@ -43,6 +59,8 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	public RPCService() throws Exception {
 		super("rpc");
+		rpcMethods = new HashMap<Pair<String, String>, RPCCallableMethod>();
+		HOST = "";
 	}
 	
 	/**
@@ -51,6 +69,29 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public void run() {
+		try {
+			String serverIP = IPFinder.localIP();
+			int tcpPort = 0;
+			mServerSocket = new ServerSocket();
+			mServerSocket.bind(new InetSocketAddress(serverIP, tcpPort));
+			mServerSocket.setSoTimeout( NetBase.theNetBase().config().getAsInt("net.timeout.granularity", 500));
+			
+			threadPool = Executors.newFixedThreadPool(NUM_THREADS);
+			
+			try {
+				TCPMessageHandler handler = new TCPMessageHandler(mServerSocket.accept());
+				handler.setMaxReadLength(Integer.MAX_VALUE);
+
+				threadPool.execute(new RCPConnection(handler));
+			} catch (SocketTimeoutException e) {
+				// this is normal.  Just loop back and see if we're terminating.
+			}
+		} catch (IOException ioe) {
+			Log.w(TAG, "Server thread exiting due to exception: " + ioe.getMessage());
+		} finally {
+			if (mServerSocket != null && !mServerSocket.isClosed())
+				try { mServerSocket.close(); } catch (IOException e) { }
+		}
 	}
 	
 	/**
@@ -63,6 +104,11 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public synchronized void registerHandler(String serviceName, String methodName, RPCCallableMethod method) throws Exception {
+		if (serviceName == null) throw new IllegalArgumentException("serviceName must be non-null");
+		if (methodName == null) throw new IllegalArgumentException("methodName must be non-null");
+		if (method == null) throw new IllegalArgumentException("method must be non-null");
+		
+		rpcMethods.put(Pair.pair(serviceName, methodName), method);
 	}
 	
 	/**
@@ -73,8 +119,8 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 * @param methodName The method name
 	 * @return The existing registration for that method of that service, or null if no registration exists.
 	 */
-	public RPCCallableMethod getRegistrationFor( String serviceName, String methodName) {
-		return null;
+	public RPCCallableMethod getRegistrationFor(String serviceName, String methodName) {
+		return rpcMethods.get(Pair.pair(serviceName, methodName));
 	}
 	
 	/**
@@ -83,11 +129,98 @@ public class RPCService extends NetLoadableService implements Runnable, RPCServi
 	 */
 	@Override
 	public int localPort() {
-		return 0;
+		return mServerSocket == null ? 0 : mServerSocket.getLocalPort();
 	}
 	
 	@Override
 	public String dumpState() {
 		return "";
+	}
+	
+	public synchronized int getIdAndInc() {
+		return currentId++;
+	}
+	
+	
+	private class RCPConnection implements Runnable {
+		private TCPMessageHandler handler;
+		
+		public RCPConnection(TCPMessageHandler handler) {
+			this.handler = handler;
+		}
+		
+		@Override
+		public void run() {
+			// Handshake
+			RPCMessage connectMsg = null;
+			try {
+				connectMsg = RPCMessage.unmarshall(handler.readMessageAsString());
+				
+				RPCMessage successMsg = new RPCNormalResponseMessage(connectMsg.id(), null);
+				handler.sendMessage(successMsg.marshall());
+			} catch (IOException | JSONException e) {
+				try {
+					RPCMessage errorMsg = new RPCErrorResponseMessage(connectMsg.id(), e.getMessage(), null);
+					handler.sendMessage(errorMsg.marshall());
+				} catch (Exception e2) { }
+				return;
+			}
+			
+			//Success. Wait for invocation.
+			RPCMessage invocationMsg = null;
+			try {
+				invocationMsg = RPCMessage.unmarshall(handler.readMessageAsString());
+				
+				if (invocationMsg instanceof RPCInvokeMessage) {
+					RPCInvokeMessage invokeMsg = (RPCInvokeMessage) invocationMsg;
+					RPCCallableMethod method = rpcMethods.get(Pair.pair(invokeMsg.app(), invokeMsg.method()));
+					if (method != null) {
+						JSONObject result = method.handleCall(invokeMsg.args());
+						
+						RPCNormalResponseMessage responseMsg = new RPCNormalResponseMessage(invokeMsg.id(), result);
+						handler.sendMessage(responseMsg.marshall());
+					} else {
+						throw new Exception("No app/method of that combination found.");
+					}
+				} else {
+					throw new Exception("Expected RPCInvokeMessage but got " + invocationMsg.getClass());
+				}
+			} catch (Exception e) {
+				try {
+					RPCMessage errorMsg = new RPCErrorResponseMessage(connectMsg.id(), e.getMessage(), (RPCCallMessage) invocationMsg);
+					handler.sendMessage(errorMsg.marshall());
+				} catch (Exception e2) { }
+				return;
+			}
+		}
+	}
+	
+	
+	private static class Pair<E, T> {
+		private E left;
+		private T right;
+		
+		public static <E, T> Pair<E, T> pair(E left, T right) {
+			return new Pair<E, T>(left, right);
+		}
+		
+		private Pair(E left, T right) {
+			this.left = left;
+			this.right = right;
+		}
+		
+		@Override
+		public int hashCode() {
+			return left.hashCode() * 11 + right.hashCode();
+		}
+		
+		@Override
+		public boolean equals(Object other) {
+			if (other instanceof Pair<?, ?>) {
+				return left.equals(((Pair<?, ?>) other).left) &&
+						right.equals(((Pair<?, ?>) other).right);				
+			}
+			return false;
+		}
 	}
 }
